feda8b7abb6788bb7955123a17b2d9f75797c87b
Tanachai Hona <tanachai_ho@wssoft.co.th>
2026-05-05 13:46:26 +0700

    FILTER ใหม่ order

--- routes/order/order.js
+++ routes/order/order.js
@@ -136,7 +136,7 @@ exports.getOrderInformation = async (req, res, next) => {
       conditions.push(`(
           NOT EXISTS (SELECT 1 FROM tbl_employee_order_type WHERE emp_code = '${act_id}' AND emp_otyp_flag = 1)
           OR tbl_order.order_type IN (
-            SELECT t2.sales_order_type 
+            SELECT t2.ord_type_code
             FROM tbl_employee_order_type t1 
             JOIN tbl_order_type t2 ON t1.ord_type_code = t2.ord_type_code 
             WHERE t1.emp_code = '${act_id}' AND t1.emp_otyp_flag = 1
